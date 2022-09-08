c93e4c12a84ec17bb6236d6e9d9f72693b85fa5d
Shivesh Tiwari <shiveshfriends@gmail.com>
2022-09-08 05:17:32 -0700

    dsfsd

---
+++ SPPROJ/src/test/java/com/example/filedemo/FileDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package src.test.java.com.example.filedemo;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FileDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
